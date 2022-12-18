005444ef700eeda0e3892a2b699e605424a67fd8
Boris <Boris@DESKTOP-0E1711G>
2022-12-18 11:54:53 +0200

    Add project files.

---
+++ HardwareStoreProject/Controllers/AccountsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using HardwareStoreProject.Models;
+
+namespace HardwareStoreProject.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        HARDWARE_DBEntities entity = new HARDWARE_DBEntities();
+       
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel credentials)
+        {
+            bool userExist = entity.UsersTbls.Any(x => x.Email == credentials.Email && x.Passcode == credentials.Password);
+            UsersTbl u = entity.UsersTbls.FirstOrDefault(x => x.Email == credentials.Email && x.Passcode == credentials.Password);
+
+            if (userExist)
+            {
+                FormsAuthentication.SetAuthCookie(u.Username, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Username or Password is wrong");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(UsersTbl userinfo)
+        {
+            entity.UsersTbls.Add(userinfo);
+            entity.SaveChanges();
+            return RedirectToAction("Login");
+            return View();
+        }
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+            return View();
+        }
+    }
+}
---
+++ HardwareStoreProject/Models/Products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HardwareStoreApp.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Product Name Required.")]
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is Required.")]
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Qty is Required.")]
+
+        public int Qty { get; set; }
+
+        public string Remarks { get; set; }
+    }
+
+    public class EFCodeFirstEntieis : DbContext
+    {
+        public DbSet<Products> products { get; set; }
+
+    }
+
+}
